0a1aeee78f8a4fdf11fd83014c3f5b60f34622a1
João Manuel <jmsmarques8213@gmail.com>
2025-05-28 10:23:21 -0300

    feat: create user model

---
+++ backend/src/models/user.ts
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+	{
+		username: {
+			type: String,
+			require: true,
+		},
+		password: {
+			type: String,
+			require: true,
+		},
+	},
+	{
+		timestamps: true,
+	}
+);
+
+const userModel = mongoose.model("users", userSchema);
+
+export default userModel;
