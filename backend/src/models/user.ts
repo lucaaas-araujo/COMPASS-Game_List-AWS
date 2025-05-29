a9a9145b198cf26bbc42838e58f65b039f0e82a5
Jessica <jessica.santosb@outlook.com>
2025-05-29 10:24:43 -0300

    feat: add confirm password to user model

--- backend/src/models/user.ts
+++ backend/src/models/user.ts
@@ -15,6 +15,10 @@ const userSchema = new mongoose.Schema(
       type: String,
       require: true,
     },
+    confirm_password: {
+      type: String,
+      require: true,
+    },
   },
   {
     timestamps: true,
