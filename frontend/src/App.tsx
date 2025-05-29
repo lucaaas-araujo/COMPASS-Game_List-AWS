90eeed8a479d7adb5d19a01a250b6e6b64bfd570
Leticia Schiavon <leticiaschiavon20@gmail.com>
2025-05-29 11:07:25 -0400

    feat:branch development

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,16 +4,16 @@ import './App.css';
 function App() {
   return (
     <Router>
-            <Routes>
-                <Route path="/login" element={<>LOGIN</>} />
-                <Route path="/register" element={<>REGISTER</>} />
-                <Route path="/home" element={<>HOME</>} />
-                <Route path="/games" element={<>GAMES</>} />
-                <Route path="/categories" element={<>CATEGORIES</>} />
-                <Route path="/platforms" element={<>PLATFORMS</>} />
-            </Routes>
-        </Router>
-  )
+      <Routes>
+        <Route path='/login' element={<>LOGIN</>} />
+        <Route path='/register' element={<>REGISTER</>} />
+        <Route path='/home' element={<>HOME</>} />
+        <Route path='/games' element={<>GAMES</>} />
+        <Route path='/categories' element={<>CATEGORIES</>} />
+        <Route path='/platforms' element={<>PLATFORMS</>} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
