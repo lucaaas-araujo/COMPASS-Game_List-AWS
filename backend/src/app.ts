65673412469cfaafeb1c640185a0fd3d27c99815
João Manuel <jmsmarques8213@gmail.com>
2025-05-29 12:33:10 -0300

    feat: create category routes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,12 +1,13 @@
 import './config/connectiondb';
 
 import express from 'express';
-import { home, user } from './routes';
+import { home, user, category } from './routes';
 
 const app = express();
 
 app.use(express.json());
 app.use(home.routes);
 app.use(user.routes);
+app.use(category.routes);
 
 export default app;
