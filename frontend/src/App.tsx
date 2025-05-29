8fb2369a9d9277a7da0707f6bda7651bb7cc5c0c
Leticia Schiavon <leticiaschiavon20@gmail.com>
2025-05-29 08:33:28 -0400

    feat: creating routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,19 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 
 function App() {
-  return <></>;
+  return (
+    <Router>
+            <Routes>
+                <Route path="/login" element={<>LOGIN</>} />
+                <Route path="/register" element={<>REGISTER</>} />
+                <Route path="/home" element={<>HOME</>} />
+                <Route path="/games" element={<>GAMES</>} />
+                <Route path="/categories" element={<>CATEGORIES</>} />
+                <Route path="/platforms" element={<>PLATFORMS</>} />
+            </Routes>
+        </Router>
+  )
 }
 
 export default App;
