ebef3b92feb033783e2ac70ed620ba15d0a61143
Jessica <jessica.santosb@outlook.com>
2025-05-30 09:59:04 -0300

    feat: add cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,15 @@
 import './config/connectiondb';
 
+import cors from 'cors';
 import express from 'express';
-import { home, user, platform, game, category } from './routes';
+
+import { category, game, home, platform, user } from './routes';
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
+
 app.use(home.routes);
 app.use(user.routes);
 app.use(game.routes);
