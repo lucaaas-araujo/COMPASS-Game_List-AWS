06219cf722b87387d3375adf22932f88acde2b20
Jessica <jessica.santosb@outlook.com>
2025-05-31 09:54:59 -0300

    feat: create select component

--- frontend/src/pages/Games.tsx
+++ frontend/src/pages/Games.tsx
@@ -1,4 +1,9 @@
 import { useEffect } from 'react';
+import {
+  Select,
+  SelectGroup,
+  SelectItem,
+} from '../components/ui/select/Select';
 import { gameServices } from '../services';
 
 export function Games() {
@@ -12,8 +17,33 @@ export function Games() {
   }, []);
 
   return (
-    <main>
+    <main style={{ paddingInline: '8rem', width: '700px' }}>
       <h3>Games</h3>
+      <Select
+        defaultValue={''}
+        variant='modal'
+        onChange={(e) => console.log(e.target.value)}>
+        <SelectItem value=''>select a fruit</SelectItem>
+        <SelectGroup>
+          <SelectItem value='apple'>Apple</SelectItem>
+          <SelectItem value='banana'>Banana</SelectItem>
+          <SelectItem value='blueberry'>Blueberry</SelectItem>
+          <SelectItem value='grapes'>Grapes</SelectItem>
+          <SelectItem value='pineapple'>Pineapple</SelectItem>
+        </SelectGroup>
+      </Select>
+      <Select defaultValue={''}>
+        <SelectItem value='' disabled>
+          select a fruit
+        </SelectItem>
+        <SelectGroup>
+          <SelectItem value='apple'>Apple</SelectItem>
+          <SelectItem value='banana'>Banana</SelectItem>
+          <SelectItem value='blueberry'>Blueberry</SelectItem>
+          <SelectItem value='grapes'>Grapes</SelectItem>
+          <SelectItem value='pineapple'>Pineapple</SelectItem>
+        </SelectGroup>
+      </Select>
     </main>
   );
 }
