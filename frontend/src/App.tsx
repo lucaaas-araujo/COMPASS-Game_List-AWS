f31a17d754d3473aadc008b48c841867b272e12a
Leticia Schiavon <leticiaschiavon20@gmail.com>
2025-06-01 09:29:23 -0400

    feat: inserted components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,6 +7,8 @@ import { Register } from './pages/auth/register/Register';
 import { Categories } from './pages/Categories';
 import { Games } from './pages/Games';
 import { Home } from './pages/home/Home';
+import { Plataforms } from './pages/platforms/Platforms';
+
 
 function App() {
   return (
@@ -18,7 +20,7 @@ function App() {
           <Route index element={<Home />} />
           <Route path='/games' element={<Games />} />
           <Route path='/categories' element={<Categories />} />
-          <Route path='/platforms' element={<>PLATFORMS</>} />
+          <Route path='/platforms' element={<Plataforms/>} />
         </Route>
       </Routes>
     </Router>
