216b12de37e5ddd19ebf1424993187ba7c2a1864
Jessica <jessica.santosb@outlook.com>
2025-06-01 18:37:22 -0300

    feat: create require auth

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,9 @@
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
 import './App.css';
 
 import Layout from './components/Layout';
@@ -9,21 +14,24 @@ import { Games } from './pages/Games';
 import { Home } from './pages/home/Home';
 import { Platform } from './pages/platform/Platform';
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
+      <Route path='/login' element={<Login />} />
+      <Route path='/register' element={<Register />} />
+      <Route path='/' element={<Layout />}>
+        <Route index element={<Home />} />
+        {/* <Route path='/games' element={<Games />} loader={requireAuth} /> */}
+        <Route path='/games' element={<Games />} />
+        <Route path='/categories' element={<Categories />} />
+        <Route path='/platforms' element={<Platform />} />
+      </Route>
+    </Route>,
+  ),
+);
+
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path='/login' element={<Login />} />
-        <Route path='/register' element={<Register />} />
-        <Route path='/' element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path='/games' element={<Games />} />
-          <Route path='/categories' element={<Categories />} />
-          <Route path='/platforms' element={<Platform />} />
-        </Route>
-      </Routes>
-    </Router>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
