9af350e8ce2f62b0c57bee78f265680e23eaf34c
Jessica <jessica.santosb@outlook.com>
2025-06-01 10:11:59 -0300

    feat: create platform context

---
+++ frontend/src/hooks/usePlatform.tsx
@@ -0,0 +1,27 @@
+import { createContext, useContext } from 'react';
+
+import type { PlatformProps } from '../types/Platform';
+
+type PlatformContextProps = {
+  allCategories: PlatformProps[];
+  duplicateCategories: PlatformProps[];
+  getAll: () => Promise<void>;
+  remove: () => Promise<void>;
+  create: (platformData: PlatformProps) => Promise<void>;
+  update: (platformData: PlatformProps) => Promise<void>;
+  platformCount: number;
+  error: boolean;
+  loading: boolean;
+};
+
+export const PlatformContext = createContext<PlatformContextProps | undefined>(
+  undefined,
+);
+
+export const usePlatform = () => {
+  const context = useContext(PlatformContext);
+  if (context === undefined) {
+    throw new Error('usePlatform must be used within a PlatformProvider');
+  }
+  return context;
+};
