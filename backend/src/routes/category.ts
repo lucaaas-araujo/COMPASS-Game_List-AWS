b1ec2b9393b3a3e918cf733b501ad4d530e667d8
Jessica <jessica.santosb@outlook.com>
2025-06-03 09:44:52 -0300

    feat: add filters to get all categories api

--- backend/src/routes/category.ts
+++ backend/src/routes/category.ts
@@ -1,10 +1,16 @@
 import { categoryController } from '@/controllers';
+import { ensureAuthentication } from '@/middleware/ensureAuthentication';
 import { Router } from 'express';
 
 const routes = Router();
 
+routes.get(
+  '/categories',
+  ensureAuthentication,
+  categoryController.getAllValidator,
+  categoryController.getAll,
+);
 routes.post('/categories', categoryController.create);
-routes.get('/categories/:user_id', categoryController.getAll);
 routes.put('/categories/:id', categoryController.update);
 routes.delete('/categories/:id', categoryController.deleteCategory);
 
