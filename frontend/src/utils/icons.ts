6b860a53d2f20aaffc956d2d2b009be80e94b630
Leticia Schiavon <leticiaschiavon20@gmail.com>
2025-06-05 15:19:47 -0400

    feat: styles componentes

--- frontend/src/utils/icons.ts
+++ frontend/src/utils/icons.ts
@@ -4,6 +4,7 @@ import platform from '../assets/chip.svg';
 import gameHover from '../assets/controller-hover.svg';
 import game from '../assets/controller.svg';
 import eye from '../assets/eye.svg';
+import eyeClosed from '../assets/eye-closed.svg';
 import homeHover from '../assets/home-hover.svg';
 import home from '../assets/home.svg';
 import compactLogo from '../assets/logo-compact.svg';
@@ -11,12 +12,14 @@ import { default as fullLogo, default as logo } from '../assets/logo.svg';
 import logoutHover from '../assets/logout-hover.svg';
 import logout from '../assets/logout.svg';
 import pen from '../assets/pen.svg';
+import penBlack from '../assets/pen-black.svg';
 import categoryHover from '../assets/pricetag-hover.svg';
 import category from '../assets/pricetag.svg';
 import starHome from '../assets/star_home.svg';
 import star from '../assets/star.svg';
 import starFilled from '../assets/star_filled.svg';
 import trash from '../assets/trash.svg';
+import trashBlack from '../assets/trash-black.svg';
 import x from '../assets/x.svg';
 import warning from '../assets/warning.svg';
 import imageController from '../../public/controller-game-list.png';
@@ -27,6 +30,7 @@ export {
   categoryHover,
   compactLogo,
   eye,
+  eyeClosed,
   fullLogo,
   game,
   gameHover,
@@ -36,11 +40,13 @@ export {
   logout,
   logoutHover,
   pen,
+  penBlack,
   platform,
   platformHover,
   star,
   starFilled,
   trash,
+  trashBlack,
   starHome,
   x,
   warning,
