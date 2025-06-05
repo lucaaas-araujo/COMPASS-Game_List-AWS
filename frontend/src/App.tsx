bedf41a6138e97a8a0952e93fc4f09a37478ce52
Jessica <jessica.santosb@outlook.com>
2025-06-05 19:53:08 -0300

    feat: redirect if logged in

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -14,13 +14,18 @@ import { Category } from './pages/category/Category';
 import { Games } from './pages/games/Games';
 import { Home } from './pages/home/Home';
 import { Platform } from './pages/platform/Platform';
+import { redirectIfLoggedIn } from './services/redirectIfLoggedIn';
 import { requireAuth } from './services/requiredAuth';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route>
-      <Route path='/login' element={<Login />} />
-      <Route path='/register' element={<Register />} />
+      <Route path='/login' element={<Login />} loader={redirectIfLoggedIn} />
+      <Route
+        path='/register'
+        element={<Register />}
+        loader={redirectIfLoggedIn}
+      />
       <Route path='/' element={<Layout />}>
         <Route index element={<Home />} loader={requireAuth} />
         <Route path='/games' element={<Games />} loader={requireAuth} />
