e0c3500a5f633677e7334aeac8d9063e8fb58d04
iyaof <lucasaraujofernandes_@hotmail.com>
2025-06-25 11:37:25 -0300

    feat: configure Docker setup with nginx, backend, and frontend services

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,7 @@
 import app from './app';
 
-const port = process.env.PORT;
+const PORT = Number(process.env.PORT) || 3000;
 
-app.listen(port, () => {
-  console.log(port);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on port: ${String(PORT)}`);
 });
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,9 +4,19 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   plugins: [react()],
   server: {
+    watch: {
+      usePolling: true,
+    },
+    host: true,
+    allowedHosts: [
+      'ec2-3-144-27-179.us-east-2.compute.amazonaws.com',
+      'localhost',
+    ],
+    strictPort: true,
+    port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:8888',
+        target: 'http://localhost:8888/',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
